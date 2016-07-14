cf9c376a9b9db51d58dd59124262b81d9a44370b
Станислав <stas249501@gmail.com>
2016-07-15 01:36:35 +0300

    maybe done task2

--- js/custom.js
+++ js/custom.js
@@ -13,7 +13,64 @@
 })(jQuery);
 
 (function ($) {
-    $('#skills ul.skills li').each(function() {
+    $('body>nav .toggle-menu').click(function () {
+        $('body').toggleClass('hide-menu');
+    });
+})(jQuery);
+
+(function ($) {
+    $('#portfolio .tags li').click(function (e) {
+        e.preventDefault();
+        $('#portfolio .tags li').removeClass('active');
+        $(this).addClass('active');
+
+        var tag = $(this).data('tag');
+        if (tag == undefined || tag == "") {
+            $('#portfolio .portfolio article').show(500);
+            return;
+        }
+
+        $('#portfolio .portfolio article:not([data-tag=' + tag + '])').hide(500);
+        $('#portfolio .portfolio article[data-tag=' + tag + ']').show(500);
+    });
+})(jQuery);
+
+(function ($) {
+    $(window).scroll(function () {
+        $('main section').each(function (i) {
+            var windscroll = $(window).scrollTop();
+            if ($(this).position().top <= windscroll) {
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i).addClass('active');
+            }
+        });
+    }).scroll();
+
+    $('nav ul a').on('click', function() {
+
+        var scrollAnchor = $(this).attr('href'),
+            scrollPoint = $(scrollAnchor).offset().top;
+
+        $('body,html').animate({
+            scrollTop: scrollPoint
+        }, 500);
+
+        return false;
+
+    });
+})(jQuery);
+
+(function($) {
+    $('.to-top').click(function (e) {
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+    });
+})(jQuery);
+
+(function ($) {
+    $('#skills ul.skills li').each(function () {
         prepareSkill($(this).find('article'));
     });
 
@@ -30,7 +87,7 @@
         } else if (skillRange > 100) {
             skillRange = 100;
         }
-        
+
 
         var skill = $('#skills ul.skills li').first().clone();
         skill.find('article').text(skillName);
@@ -43,4 +100,4 @@
         var range = $itemArticle.data('value');
         $itemArticle.css('width', range + '%');
     }
-})(jQuery)
+})(jQuery);
